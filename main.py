3130ec924bff5bae2c7bb5adc2a0931d04f63fb3
Vlai22 <86800576+Vlai22@users.noreply.github.com>
2024-04-28 23:09:18 +0300

    Сделанный полный скан

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ cube_color_pos[6][9] = {{0,0,0,0,COLOR[0],0,0,0,0},{0,0,0,0,COLOR[1],0,0,0,0},
                     {0,0,0,0,COLOR[4],0,0,0,0},{0,0,0,0,COLOR[5],0,0,0,0}} 
 #массив соотносящихся значений которые выдаёт color_sensor с массивом COLOR то есть 
 #число выдаваемое функцией сканирования цвета соответствует цвету из массива 
-color_color_enser[] = {10,8,6,9,3,7}   
+color_color_senser[] = {10,8,6,9,3,7}   
 
 #констуркиця await подразумевает собой что наша программа откладывает выполнение программы 
 #до тех пор пока мы не выполним какую либо функцию или что либо
@@ -49,22 +49,50 @@ async def scan():
         rotate()
      rotat(90)
      turn()
+async def scan_3x3(i):
+    await motor.run_for_degrees(port.B, -70, 500)#перемещаемся на кубик под центром
+    await cube_color_pos[i][3] == color_sensor.color(port.A)#сканируем и записываем на соответсвующую грень 
+    await rotate(45)#вращаем до крайнего куба
+    await motor.run_for_degrees(port.B, -20, 500)#корректируем положение датчика цвета
+    await cube_color_pos[i][8] == color_sensor.color(port.A)#сканируем и записываем на соответстсвующую грань 
+    await rotate(45)#докручиваем до следующей стороны
+    await motor.run_for_degrees(port.B, 20, 500)#возвращаем датчик цвета в положение чтения нижнего куба от центра
+    await cube_color_pos[i][7] == color_sensor.color(port.A)#сканируем и записываем на соответстсвующую грань
+    await rotate(45)#вращаем до крайнего куба
+    await motor.run_for_degrees(port.B, -20, 500)#корректируем положение датчика цвета
+    await cube_color_pos[i][6] == color_sensor.color(port.A)#сканируем и записываем на соответстсвующую грань
+    await rotate(45)#докручиваем до следующей стороны
+    await motor.run_for_degrees(port.B, 20, 500)#возвращаем датчик цвета в положение чтения нижнего куба от центра
+    await cube_color_pos[i][5] == color_sensor.color(port.A)#сканируем и записываем на соответстсвующую грань
+    await rotate(45)#вращаем до крайнего куба
+    await motor.run_for_degrees(port.B, -20, 500)#корректируем положение датчика цвета
+    await cube_color_pos[i][0] == color_sensor.color(port.A)#сканируем и записываем на соответстсвующую грань
+    await rotate(45)#докручиваем до следующей стороны
+    await motor.run_for_degrees(port.B, 20, 500)#возвращаем датчик цвета в положение чтения нижнего куба от центра
+    await cube_color_pos[i][1] == color_sensor.color(port.A)#сканируем и записываем на соответстсвующую грань
+    await rotate(45)#вращаем до крайнего куба
+    await motor.run_for_degrees(port.B, -20, 500)#корректируем положение датчика цвета
+    await cube_color_pos[i][2] == color_sensor.color(port.A)#сканируем и записываем на соответстсвующую грань
+    await rotate(45)#докручиваем до следующей стороны
+    await motor.run_for_degrees(port.B, -380, 500)#возвращаем датчик цвета в положение чтения нижнего куба от центра
 
-async def scan_vlad():
+async def scan_full():
     now_color = None
-    name_color = None
-    await motor.run_for_degrees(port.B, 470, 500)#выставляем мотор на центер кубика рубика
-    await now_color = color_sensor.color(port.A)#переедаём в переменную полученый с сенсора
-    #данные, они отображаются в виде цифры по этому необходимо их проеобраховать в название
     await for i in range(len(COLOR)):
-            if now_color[i] is COLOR[i]:
-                name_color = COLOR[i]
-    await for i in range(LEN(COLOR)):
-    
-
-    
-    
-
+        await motor.run_for_degrees(port.B, 470, 500)#выставляем мотор на центер кубика рубика
+        await now_color = color_sensor.color(port.A)#переедаём в переменную полученый с сенсора
+        #данные, они отображаются в виде цифры по этому необходимо их проеобраховать в название
+        await for j in range(len(COLOR)):#перечисление всех цветов
+                if now_color == color_color_senser[j]:#поиск номера цвета с сентера с нынешним цветом
+                    scan_3x3(j)
+    await for i in range(6):
+            for j in range(9):
+                if j != 4:
+                    for k in range(6):
+                        if cube_color_pos[i][j] is color_color_senser[k]:
+                            cube_color_pos[i][j] = COLOR[k]
+        
+        
 
 async def main():
     runloop.run(rotate(90))
